f8342ed8a29b5858865d40fcb5ee0cb99ee48499
Robert Nilsen <padletut@gmail.com>
2025-06-10 02:39:54 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: /home/padletut/ecudeck/tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Include all files in the `src` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
