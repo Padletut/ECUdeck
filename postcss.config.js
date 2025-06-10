7347134379e7b55a2db7aa41865b7e23a907f72b
Robert Nilsen <padletut@gmail.com>
2025-06-10 02:39:32 +0200

    Add PostCSS configuration with Tailwind CSS and Autoprefixer plugins

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: /home/padletut/ecudeck/postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use the correct Tailwind CSS PostCSS plugin
+    autoprefixer: {}, // Ensure autoprefixer is included
+  },
+};
